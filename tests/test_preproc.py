ee8cec177bdc8786a8f045918515c06dc53d99c5
ivo <ivo@danihelka.net>
2009-12-20 23:11:14 +0100

    Tested the used_fields and the generalize() method.

--- tests/test_preproc.py
+++ tests/test_preproc.py
@@ -6,13 +6,25 @@ from soko.struct import modeling, preproc
 
 def test_get_children():
     rules = [PushRule()]
-    s = modeling.immutablize("""\
+    s = _parse_field("""\
 #   #
  $ .#
    @#
     #
-#####""".splitlines())
+#####""")
 
     end_states, used_cells = preproc.detect_end_states(s, rules)
     assert_equal(6, len(end_states))
+
+    assert_equal(_parse_field("""\
+%   %
+ $ .#
+   @#
+    #
+%###%"""), preproc.generalize(s, used_cells))
+
+
+def _parse_field(input):
+    lines = input.splitlines()
+    return  modeling.immutablize(lines)
 
