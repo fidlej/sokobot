d1da4535fb0cc5aae91b64eab147ac3fc887c6d7
ivo <ivo@danihelka.net>
2010-02-17 19:17:52 +0100

    Counting the number of visits.

--- soko/solver/mc.py
+++ soko/solver/mc.py
@@ -17,13 +17,45 @@ class McSolver(Solver):
 
 
 def _sample(env, s):
+    """Returns a random path to the goal or None.
+    """
+    memory = Memory()
     path = []
     cost = env.estim_cost(s)
     while cost > 0:
-        a = random.choice(env.get_actions(s))
+        a = _choose_action(env, s, memory)
+        if a is None:
+            return None
+
         path.append(a)
+        memory.inc_num_visits(s)
         s = env.predict(s, a)
         cost = env.estim_cost(s)
 
     return path
+
+
+class Memory(object):
+    def __init__(self):
+        self.visits = {}
+    def get_num_visits(self, s):
+        return self.visits.get(s, 0)
+    def inc_num_visits(self, s):
+        self.visits[s] = self.visits.get(s, 0) + 1
+
+
+def _choose_action(env, s, memory):
+    choices = []
+    for a in env.get_actions(s):
+        next_s = env.predict(s, a)
+        num_visits = memory.get_num_visits(next_s)
+        choices.append((num_visits, a))
+
+    if not choices:
+        return None
+
+    #TODO: choose at random, if the num_visits are equal
+    num_visits, a = min(choices)
+    #TODO: return also None when num_visits > k.
+    return a
 
