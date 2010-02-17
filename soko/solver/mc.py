24ca87e9582d0b663a76dec6741d6d8164b3477f
ivo <ivo@danihelka.net>
2010-02-17 23:05:27 +0100

    Allowed to nest the MC searches.

--- soko/solver/mc.py
+++ soko/solver/mc.py
@@ -3,6 +3,7 @@ import random
 import math
 
 from soko.solver.solver import Solver
+from soko.visual.lengthvisualizer import calc_path_cost
 
 MAX_NUM_VISITS = 10
 
@@ -13,14 +14,19 @@ class McSolver(Solver):
         """Returns a solution as a list of actions.
         """
         s = env.init()
-        info = SearchInfo(env)
-        path = _sample(info, s)
+        info = _SearchInfo(env)
+        level = 1
+        path = _nested(info, s, level)
+        #path = _sample(info, s)
         return path
 
 
-class SearchInfo(object):
+class _SearchInfo(object):
     def __init__(self, env):
         self.env = env
+
+class _Memory(object):
+    def __init__(self):
         self.visits = {}
     def get_num_visits(self, s):
         return self.visits.get(s, 0)
@@ -28,35 +34,76 @@ class SearchInfo(object):
         self.visits[s] = self.visits.get(s, 0) + 1
 
 
+def _nested(info, s, level):
+    env = info.env
+    path = []
+    while not _is_goal(env, s):
+        a = _choose_best_action(info, s, level)
+        if a is None:
+            return None
+
+        path.append(a)
+        s = env.predict(s, a)
+
+    return path
+
+def _choose_best_action(info, s, level):
+    """Returns the best known action in the given state.
+    It returns None if the problem seems unsolvable.
+    """
+    env = info.env
+    min_cost = None
+    best_action = None
+    for a in env.get_actions(s):
+        next_s = env.predict(s, a)
+        #TODO: use also the best previously known score for next_s
+        if level == 1:
+            path = _sample(info, next_s)
+        else:
+            path = _nested(info, next_s, level - 1)
+
+        if path is None:
+            continue
+
+        cost = calc_path_cost(path)
+        if min_cost is None or cost < min_cost:
+            min_cost = cost
+            best_action = a
+
+    return best_action
+
+def _is_goal(env, s):
+    return env.estim_cost(s) == 0
 
 def _sample(info, s):
     """Returns a random path to the goal or None.
     """
+    memory = _Memory()
     env = info.env
     path = []
-    cost = env.estim_cost(s)
-    while cost > 0:
-        a = _choose_action(info, s)
+    while not _is_goal(env, s):
+        a = _choose_random_action(env, s, memory)
         if a is None:
             return None
-        if info.get_num_visits(s) > MAX_NUM_VISITS:
+        if memory.get_num_visits(s) > MAX_NUM_VISITS:
             return None
 
         path.append(a)
-        info.inc_num_visits(s)
+        memory.inc_num_visits(s)
         s = env.predict(s, a)
-        cost = env.estim_cost(s)
 
     return path
 
 
-def _choose_action(info, s):
-    env = info.env
+def _choose_random_action(env, s, memory):
+    """Chooses a random action.
+    It gives more probability to less visited next states.
+    """
     weights = []
     actions =  env.get_actions(s)
     for a in actions:
         next_s = env.predict(s, a)
-        num_visits = info.get_num_visits(next_s)
+        num_visits = memory.get_num_visits(next_s)
         weights.append(1.0/(num_visits + 1))
 
     if not weights:
