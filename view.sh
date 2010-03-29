210ec7be90235003e97846d3f04f85361c1c1291
ivo <ivo@danihelka.net>
2010-03-29 14:49:34 +0200

    Allowed to use python when python2.6 is not available.

--- view.sh
+++ view.sh
@@ -4,5 +4,5 @@
 solution_file=`mktemp /tmp/sokobot.solution.XXXXXXXXXX` || exit 1
 trap 'rm -f "$solution_file"' 0 1 2 3 15
 if ./solve.py --vs "$@" >"$solution_file" ; then
-    python2.6 ./utils/scroll.py "$solution_file"
+    `which python2.6 python | head -1` ./utils/scroll.py "$solution_file"
 fi
